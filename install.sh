57cda8f7bb48d0626ca0cf43e89b0dac649d8029
Celio <celio@opixido.com>
2025-04-16 14:56:58 +0200

    Fix install

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir -p .ddev/commands/host
-cp "$(dirname "$0")/commands/host/phploy" .ddev/commands/host/phploy
-cp "$(dirname "$0")/docker-compose.phploy.yaml" .ddev/docker-compose.phploy.yaml
-cp "$(dirname "$0")/Dockerfile" .ddev/Dockerfile.phploy
-
-
-echo "✅ Add-on phploy installé. Redémarre DDEV avec 'ddev restart'."
